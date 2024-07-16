a7bdcec309247a54b59c4e128b531e16967a66d1
OliwierSellig <oliwierandrzej.sellig@gmail.com>
2024-07-16 17:43:44 +0200

    feat: add makrdown && image files

--- next/src/global/constants.ts
+++ next/src/global/constants.ts
@@ -1,4 +1,10 @@
 /**
+ * Global declaration of domain name of the website. Be aware of the protocol and www or non-www prefix.
+ * @constant
+ */
+export const DOMAIN: string = 'https://laik-knows.pl/';
+
+/**
  * Global declaration of page language.
  * @constant
  */
