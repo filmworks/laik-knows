b7f4a78526fda890c6575cc058bf595edf7b1d4c
OliwierSellig <oliwierandrzej.sellig@gmail.com>
2024-07-17 17:36:35 +0200

    feat: clear before production

--- next/src/app/page.tsx
+++ next/src/app/page.tsx
@@ -1,9 +1,5 @@
 import Button from '@/components/ui/Button';
 
 export default function Home() {
-  return (
-    <div>
-      <Button href='https://google.com'>Zapisz się</Button>
-    </div>
-  );
+  return <div></div>;
 }
