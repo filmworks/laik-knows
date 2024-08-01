e95fec979f7fea56e97ac7e18fc97eaa867baa61
OliwierSellig <oliwierandrzej.sellig@gmail.com>
2024-08-01 20:02:55 +0200

    feat: create robots, sitemap, mainfest (no icons yet)

---
+++ next/src/app/manifest.ts
@@ -0,0 +1,26 @@
+import { MetadataRoute } from 'next';
+import { BACKGROUND_COLOR, DEFAULT_DESCRIPTION, DEFAULT_TITLE, SHORT_TITLE, THEME_COLOR } from '@/global/constants';
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: DEFAULT_TITLE,
+    short_name: SHORT_TITLE,
+    description: DEFAULT_DESCRIPTION,
+    start_url: '/',
+    display: 'standalone',
+    background_color: BACKGROUND_COLOR,
+    theme_color: THEME_COLOR,
+    // icons: [
+    //   {
+    //     src: `${DOMAIN}/icons/icon-192x192.png`,
+    //     sizes: '192x192',
+    //     type: 'image/png',
+    //   },
+    //   {
+    //     src: `${DOMAIN}/icons/icon-512x512.png`,
+    //     sizes: '512x512',
+    //     type: 'image/png',
+    //   },
+    // ],
+  };
+}
--- next/src/global/constants.ts
+++ next/src/global/constants.ts
@@ -5,16 +5,41 @@
 export const DOMAIN: string = 'https://laik-knows.pl/';
 
 /**
- * Global declaration of page language.
+ * Global default title.
  * @constant
  */
-export const LOCALE: string = 'pl';
+export const DEFAULT_TITLE: string = 'Laik Knows';
 
 /**
  * Global default title.
  * @constant
  */
-export const DEFAULT_TITLE: string = 'Laik Knows';
+export const SHORT_TITLE: string = 'LaikKnows';
+
+/**
+ * Global description.
+ * @constant
+ */
+export const DEFAULT_DESCRIPTION: string =
+  'Odkryj tajniki tworzenia wysokiej jakości wideo, które przyciągają klientów i zwiększają sprzedaż. Zdobądź niezbędne umiejętności, aby wyróżnić się w branży beauty.';
+
+/**
+ * Global declaration of themeColor in HEX format.
+ * @constant
+ */
+export const THEME_COLOR: string = '#101010';
+
+/**
+ * Global declaration of backgroundColor in HEX format.
+ * @constant
+ */
+export const BACKGROUND_COLOR: string = '#000';
+
+/**
+ * Global declaration of page language.
+ * @constant
+ */
+export const LOCALE: string = 'pl';
 
 /**
  * Declaration of global easing.
