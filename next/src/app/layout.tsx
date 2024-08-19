c65191960c1d9b2b01694b632edd54aec12fb11c
OliwierSellig <oliwierandrzej.sellig@gmail.com>
2024-08-19 18:36:44 +0200

    faet: add GTM

--- next/src/app/layout.tsx
+++ next/src/app/layout.tsx
@@ -1,10 +1,10 @@
-// import { GoogleTagManager } from '@next/third-parties/google';
+import { GoogleTagManager } from '@next/third-parties/google';
 import Script from 'next/script';
 import SchemaOrganization from '@/global/Schema/Organization';
 import { LOCALE } from '@/global/constants';
 import { RedHatDisplay } from '@/global/fonts';
 import '@/global/global.scss';
-// import CookieConsent from '@/components/global/CookieConsent';
+import CookieConsent from '@/components/global/CookieConsent';
 import Footer from '@/components/global/Footer';
 import Header from '@/components/global/Header';
 
@@ -19,11 +19,12 @@ export default function RootLayout({
         <Header />
         <main id='main'>{children}</main>
         <Footer />
-        {/* <CookieConsent /> */}
+        <CookieConsent />
         <SchemaOrganization />
         <Script async src='https://player.vimeo.com/api/player.js' strategy='beforeInteractive' />
       </body>
       {/* {process.env.NODE_ENV === 'production' && <GoogleTagManager gtmId='GTM-58CFBWW3' />} */}
+      <GoogleTagManager gtmId='GTM-M6FZ4BW6' />
     </html>
   );
 }
