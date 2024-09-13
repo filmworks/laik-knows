5ab18606df5e1642ee9aec4042cb2c2813aae297
OliwierSellig <oliwierandrzej.sellig@gmail.com>
2024-09-13 14:05:19 +0200

    feat: change auth from middleware to page

--- next/src/app/lesson/page.tsx
+++ next/src/app/lesson/page.tsx
@@ -1,3 +1,4 @@
+import { notFound } from 'next/navigation';
 import sanityFetch from '@/utils/sanity.fetch';
 import { QueryMetadata } from '@/global/Seo/query-metadata';
 import Components, { ComponentTypes, Components_Query } from '@/components/Components';
@@ -13,8 +14,15 @@ const query = async (): Promise<{ content: ComponentTypes[] }> => {
   });
 };
 
-export default async function Lesson() {
+export default async function Lesson({ searchParams }: { searchParams: { token: string } }) {
+  if (!searchParams.token) notFound();
+
+  const isValidToken = await verifyTokenWithMailerLite(searchParams.token);
+
+  if (!isValidToken) notFound();
+
   const { content } = await query();
+
   return (
     <>
       <Components data={content} />
@@ -25,3 +33,26 @@ export default async function Lesson() {
 export async function generateMetadata() {
   return await QueryMetadata({ name: 'Lesson_Page', path: '' });
 }
+
+const verifyTokenWithMailerLite = async (token: string): Promise<boolean> => {
+  try {
+    const res = await fetch('https://api.mailerlite.com/api/v2/subscribers', {
+      headers: {
+        'X-MailerLite-ApiKey': process.env.MAILERLITE_API_KEY!,
+        'Content-Type': 'application/json',
+      },
+    });
+
+    const response = await res.json();
+
+    const subscriber = response.find(
+      (sub: { fields: [{ key: string; value: string }] }) =>
+        sub.fields.find(field => field.key === 'user_token')?.value === token
+    );
+
+    return !!subscriber;
+  } catch (error) {
+    console.error('Wystąpił problem z weryfikacją tokenu', error);
+    return false;
+  }
+};
