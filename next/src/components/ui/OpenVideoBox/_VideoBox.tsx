e0676ed3efa4a84ab6696e4aee4eff066e23aff2
filmworks <filmworkspolska@gmail.com>
2025-03-31 10:52:08 +0200

    Update _VideoBox.tsx

--- next/src/components/ui/OpenVideoBox/_VideoBox.tsx
+++ next/src/components/ui/OpenVideoBox/_VideoBox.tsx
@@ -1,17 +1,27 @@
 'use client';
 
 import Img from '../image';
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import styles from './OpenVideoBox.module.scss';
 import { VideoBoxTypes } from './OpenVideoBox.types';
 
 export default function VideoBox({ video, img, sizes, videoIcon }: VideoBoxTypes) {
   const [isOpen, setIsOpen] = useState<boolean>(false);
   const [isLoading, setIsLoading] = useState<boolean>(false);
+  const iframeRef = useRef<HTMLIFrameElement | null>(null);
 
   const handleOpen = () => {
     setIsOpen(true);
     setIsLoading(true);
+  };
+
+  const handlePause = () => {
+    if (iframeRef.current) {
+      iframeRef.current.contentWindow?.postMessage(
+        { method: 'pause' },
+        '*'
+      );
+    }
   };
 
   return (
@@ -26,15 +36,22 @@ export default function VideoBox({ video, img, sizes, videoIcon }: VideoBoxTypes
           <div>{videoIcon}</div>
         </button>
       )}
+
       {isOpen && (
         <div className={styles.video}>
           <iframe
+            ref={iframeRef}
             onLoad={() => setIsLoading(false)}
             title='Film kursantki'
-            src={`https://player.vimeo.com/video/${video}?h=8f0ffe497b&autoplay=1&loop=1&controls=1&title=0&byline=0&portrait=0&dnt=1`}
+            src={`https://player.vimeo.com/video/${video}?h=8f0ffe497b&autoplay=1&controls=1&title=0&byline=0&portrait=0&dnt=1`}
             allow='autoplay; fullscreen'
             allowFullScreen
           ></iframe>
+
+          <button onClick={handlePause} className={styles.pauseButton}>
+            ⏸️ Pauza
+          </button>
+
           <div className={styles.loader}>
             <div />
           </div>
@@ -43,4 +60,3 @@ export default function VideoBox({ video, img, sizes, videoIcon }: VideoBoxTypes
     </div>
   );
 }
-
